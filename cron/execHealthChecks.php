48d056a03727a7400cdb2e98fdec4c0870797eb3
Andrew Rivett <veggiefrog@gmail.com>
2011-12-09 08:49:05 -0500

    Added stub for cron health Checks.

---
+++ cron/execHealthChecks.php
@@ -0,0 +1,15 @@
+<?php
+//
+// Description
+// -----------
+//
+// Arguments
+// ---------
+//
+function ciniki_healthchecks_exec($ciniki, $cronjob) {
+	//
+	// Load all the health checks we can find
+	//
+
+}
+?>
